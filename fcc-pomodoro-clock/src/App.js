e4435ca5bb89bd4ca94e188f1cb94b21b2311979
Anton G Neuhold Jr <agneuhold@gmail.com>
2018-11-24 22:26:18 -0800

    Basic outline added The basic outline for the pomodoro clock was added. No functionality is included yet.

--- fcc-pomodoro-clock/src/App.js
+++ fcc-pomodoro-clock/src/App.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+      minutesLeft: "25",
+      secondsLeft: "00",
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1>Pomodoro Clock</h1>
+        <div id="break-label">Break Length</div>
+        <div id="break-length">5</div>
+        <button id="break-increment">Break Increment</button>
+        <button id="break-decrement">Break Decrement</button>
+        <div id="sesson-label">Session Length</div>
+        <div id="session-length">25</div>
+        <button id="session-increment">Session Increment</button>
+        <button id="session-decrement">Session Decrement</button>
+        <div id="timer-label">Timer</div>
+        <div id="time-left">
+          {this.state.minutesLeft}:{this.state.secondsLeft}
+        </div>
+        <button id="start_stop">Start/Stop</button>
+        <button id="reset">Reset</button>
       </div>
     );
   }
