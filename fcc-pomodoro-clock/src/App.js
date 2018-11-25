a23cce265b4476dcf4a0ec10a41908e141d4e6d3
Anton G Neuhold Jr <agneuhold@gmail.com>
2018-11-24 23:25:40 -0800

    Buttons work! Some basic functionality of the buttons were added 🙂.

--- fcc-pomodoro-clock/src/App.js
+++ fcc-pomodoro-clock/src/App.js
@@ -10,19 +10,57 @@ class App extends React.Component {
       minutesLeft: "25",
       secondsLeft: "00",
     }
+
+    this.incrementSessionLength = this.incrementSessionLength.bind(this);
+    this.decrementSessionLength = this.decrementSessionLength.bind(this);
+    this.incrementBreakLength = this.incrementBreakLength.bind(this);
+    this.decrementBreakLength = this.decrementBreakLength.bind(this);
   }
+
+  incrementSessionLength() {
+    this.setState(previousState => ({
+      sessionLength: previousState.sessionLength + 1,
+    }))
+  }
+
+  decrementSessionLength() {
+    this.setState(previousState => ({
+      sessionLength: previousState.sessionLength - 1,
+    }))
+  }
+
+  incrementBreakLength() {
+    this.setState(previousState => ({
+      breakLength: previousState.breakLength + 1,
+    }))
+  }
+
+  decrementBreakLength() {
+    this.setState(previousState => ({
+      breakLength: previousState.breakLength - 1,
+    }))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Pomodoro Clock</h1>
         <div id="break-label">Break Length</div>
-        <div id="break-length">5</div>
-        <button id="break-increment">Break Increment</button>
-        <button id="break-decrement">Break Decrement</button>
+        <div id="break-length">{this.state.breakLength}</div>
+        <button id="break-increment" onClick={this.incrementBreakLength}>
+          Break Increment
+        </button>
+        <button id="break-decrement" onClick={this.decrementBreakLength}>
+          Break Decrement
+        </button>
         <div id="sesson-label">Session Length</div>
-        <div id="session-length">25</div>
-        <button id="session-increment">Session Increment</button>
-        <button id="session-decrement">Session Decrement</button>
+        <div id="session-length">{this.state.sessionLength}</div>
+        <button id="session-increment" onClick={this.incrementSessionLength}>
+          Session Increment
+        </button>
+        <button id="session-decrement" onClick={this.decrementSessionLength}>
+          Session Decrement
+        </button>
         <div id="timer-label">Timer</div>
         <div id="time-left">
           {this.state.minutesLeft}:{this.state.secondsLeft}
