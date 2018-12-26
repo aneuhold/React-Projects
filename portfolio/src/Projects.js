eb6055aa9cd9a7ce4b893569be17cc986ed87e37
Anton G Neuhold Jr <agneuhold@gmail.com>
2018-12-26 11:23:30 -0800

    Add another project Added another project and finalized the object structure for each project for now.

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -4,6 +4,18 @@ const projects = [
     heading: 'React Drum Machine',
     info: 'A drum machine built with react',
     demoLink: 'https://aneuhold-drum-machine.netlify.com/',
+    codeLink: 'https://github.com/aneuhold/React-Projects/tree/master/fcc-drum-machine',
+    thumbnailUrl: 'https://i.imgur.com/Y7N4jRO.png',
+    thumbnailDescription: 'React Drum Machine Thumbnail.',
+  },
+  {
+    name: 'React Calculator',
+    heading: 'React Calculator',
+    info: 'A calculator built with react',
+    demoLink: 'https://aneuhold-calculator.netlify.com/',
+    codeLink: 'https://github.com/aneuhold/React-Projects/tree/master/fcc-calculator',
+    thumbnailUrl: 'https://i.imgur.com/Jn7zDGy.png',
+    thumbnailDescription: 'React Cacluator Thumbnail.',
   },
 ];
 
