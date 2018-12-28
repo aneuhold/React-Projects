acf7c18fce75ce1a2e39f3e49c58bd14d0ff2a4b
Anton Neuhold Jr <agneuhold@gmail.com>
2018-12-28 14:27:41 -0800

    Add material icon library Added material icon library and a trash icon back onto the todo view

--- todo-list/src/components/ToDoView.js
+++ todo-list/src/components/ToDoView.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { IconButton } from '@material-ui/core';
 import '../components-css/ToDoView.css';
 
 const ToDoView = props => (
   <div className="ToDoView">
     {props.toDo.title}
+    <IconButton>
+      <DeleteIcon />
+    </IconButton>
   </div>
 );
 
